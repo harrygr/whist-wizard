42366c31f7b280b4d293dbe5c3ada0c6198e7edb
Harry <harry@grumbar.me>
2025-04-05 08:35:00 +0000

    Add drag-and-drop functionality to player list and update state management

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -1,10 +1,60 @@
 import { Player } from "../GameState";
+import React from "react";
+import { Array, pipe, Option } from "effect";
+import classNames from "classnames";
+import {
+  closestCenter,
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  useSortable,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface Props {
   players: Player[];
+  setPlayers: (players: Player[]) => void;
   removePlayer: (id: string) => void;
 }
-export const PlayerList = ({ players, removePlayer }: Props) => {
+export const PlayerList = ({ players, setPlayers, removePlayer }: Props) => {
+  const moveCard = (dragIndex: number, hoverIndex: number) => {
+    setPlayers(
+      pipe(
+        players,
+        Array.remove(dragIndex),
+        Array.insertAt(hoverIndex, players[dragIndex]),
+        Option.getOrElse(() => players)
+      )
+    );
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const renderPlayer = React.useCallback((player: Player, index: number) => {
+    return (
+      <PlayerListItem
+        player={player}
+        key={player.id}
+        index={index}
+        moveCard={moveCard}
+        removePlayer={() => removePlayer(player.id)}
+      />
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <h2 className="text-xl mb-4">Players</h2>
@@ -12,22 +62,72 @@ export const PlayerList = ({ players, removePlayer }: Props) => {
         Sort the players in the order they are sat, with the first player being
         the dealer for the first round.
       </p>
-      <ul className="space-y-2">
-        {players.map((player) => (
-          <li
-            key={player.id}
-            className="border border-stone-300 py-2 px-4 rounded-lg"
-          >
-            {player.name}{" "}
-            <button
-              onClick={() => removePlayer(player.id)}
-              className="cursor-pointer"
-            >
-              X
-            </button>
-          </li>
-        ))}
-      </ul>
+      <DndContext
+        sensors={sensors}
+        collisionDetection={closestCenter}
+        onDragEnd={({ active, over }) => {
+          if (active.id !== over?.id) {
+            const oldIndex = players.findIndex((p) => p.id === active.id);
+            const newIndex = players.findIndex((p) => p.id === over?.id);
+
+            moveCard(oldIndex, newIndex);
+          }
+        }}
+      >
+        <SortableContext items={players} strategy={verticalListSortingStrategy}>
+          <ul className="space-y-2">
+            {players.map((player, index) => renderPlayer(player, index))}
+          </ul>
+        </SortableContext>
+      </DndContext>
     </div>
+  );
+};
+
+interface PlayerListItemProps {
+  player: Player;
+  index: number;
+  moveCard: (dragIndex: number, hoverIndex: number) => void;
+  removePlayer: (id: string) => void;
+}
+
+const PlayerListItem = ({ player, removePlayer }: PlayerListItemProps) => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: player.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <li
+      ref={setNodeRef}
+      key={player.id}
+      className={classNames(
+        "border border-stone-300 py-2 px-4 rounded-lg bg-white flex justify-between gap-4",
+        { "shadow-lg opacity-80 z-50 relative": isDragging }
+      )}
+      style={style}
+    >
+      {player.name}{" "}
+      <div className="flex items-center gap-2">
+        <button
+          onClick={() => removePlayer(player.id)}
+          className="cursor-pointer"
+        >
+          🗑️
+        </button>
+        <button {...attributes} {...listeners} className="cursor-move">
+          ⬜️
+        </button>
+      </div>
+    </li>
   );
 };
--- src/components/PlayerSetup.tsx
+++ src/components/PlayerSetup.tsx
@@ -1,33 +1,86 @@
 import React from "react";
-import { playerReducer } from "../PlayerState";
 import { AddPlayerForm } from "./AddPlayerForm";
-import { PlayerList } from "./PlayerList";
 import { NonEmptyArray, isNonEmptyArray } from "effect/Array";
 import { Player } from "../GameState";
 import { Button } from "./Button";
+import { PlayerList } from "./PlayerList";
 
 interface Props {
   startGame: (players: NonEmptyArray<Player>) => void;
 }
 
+const initialPlayers = [
+  {
+    id: "1",
+    name: "Player 1",
+  },
+  {
+    id: "2",
+    name: "Player 2",
+  },
+  { id: "3", name: "Player 3" },
+];
+
+interface State {
+  players: Player[];
+  error: string | null;
+}
+
+type Action =
+  | { type: "setPlayers"; players: Player[] }
+  | { type: "setError"; error: string | null }
+  | { type: "addPlayer"; player: Player }
+  | { type: "removePlayer"; id: string };
+
+const reducer: React.Reducer<State, Action> = (state, action) => {
+  if (action.type === "setPlayers") {
+    return { error: null, players: action.players };
+  }
+  if (action.type === "setError") {
+    return { ...state, error: action.error };
+  }
+  if (action.type === "addPlayer") {
+    return { error: null, players: [...state.players, action.player] };
+  }
+  if (action.type === "removePlayer") {
+    return {
+      ...state,
+      players: state.players.filter((player) => player.id !== action.id),
+    };
+  }
+  return state;
+};
+
 export const PlayerSetup = ({ startGame }: Props) => {
-  const [players, dispatch] = React.useReducer(playerReducer, []);
+  const [state, dispatch] = React.useReducer(reducer, {
+    players: initialPlayers,
+    error: null,
+  });
 
   return (
     <div className="max-w-screen-sm mx-auto space-y-4">
       <AddPlayerForm
         onAddPlayer={(player) => dispatch({ type: "addPlayer", player })}
       />
+
       <PlayerList
-        players={players}
+        players={state.players}
+        setPlayers={(players) => dispatch({ type: "setPlayers", players })}
         removePlayer={(id) => dispatch({ type: "removePlayer", id })}
       />
-
+      {state.error && (
+        <div className="text-red-500 text-sm my-2">{state.error}</div>
+      )}
       <Button
         type="button"
         onClick={() => {
-          if (isNonEmptyArray(players)) {
-            startGame(players);
+          if (state.players.length < 2) {
+            dispatch({ type: "setError", error: "Add at least 2 players" });
+
+            return;
+          }
+          if (isNonEmptyArray(state.players)) {
+            startGame(state.players);
           }
         }}
       >
